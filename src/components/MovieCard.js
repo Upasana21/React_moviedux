760e60f838708260b4413b14f861bbfba251240f
upasana <bhandariupasana2111@gmail.com>
2025-03-05 22:55:41 +1100

    dynamic color rating

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React from "react";
 
 export default function MovieCard({ movie }) {
     const handleError = (e) => {
-        e.target.src = "images/default.jpg"
-    }
+        e.target.src = "images/default.jpg";
+    };
+
+    const getRatingClass = (rating) => {
+        if (rating > 8) return "rating-good";
+        else if (rating >= 5 && rating < 8) return "rating-ok";
+        else return "rating-bad";
+    };
+
     return (
         <div>
             <div key={movie.id} className="movie-card">
-                <img src={`images/${movie.image}`} alt={movie.title} onError={handleError} />
+                <img
+                    src={`images/${movie.image}`}
+                    alt={movie.title}
+                    onError={handleError}
+                />
                 <div className="movie-card-info">
                     <h3 className="movie-card-title">{movie.title}</h3>
                     <p className="movie-card-genre">{movie.genre}</p>
-                    <p className="movie-card-rating">{movie.rating}</p>
+                    <p className={`movie-card-rating ${getRatingClass(movie.rating)}`}>
+                        {movie.rating}
+                    </p>
                 </div>
             </div>
         </div>
